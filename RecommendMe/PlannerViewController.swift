12ae3b35680bc499095806a7b46afd481e2a50ed
jgebbeken <jgebbeken@gmail.com>
2016-03-22 15:20:13 -0700

    Added view controllers for the following: Main Menu, Planner, and Advance Filter View Controller.

---
+++ RecommendMe/PlannerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PlannerViewController.swift
+//  RecommendMe
+//
+//  Created by Josh Gebbeken on 3/22/16.
+//  Copyright © 2016 Team Recommend. All rights reserved.
+//
+
+import UIKit
+
+class PlannerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
